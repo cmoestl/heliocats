703ba6fef854621cc4c82756737224c4b2a5a5d2
cmoestl <chris.moestl@outlook.com>
2023-07-07 10:01:04 +0200

    change to icme_rate_web

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ input parameters for heliocats
 #for in situ data folder
 
 #local
-#data_path='data/' 
-#data_path_ML='data/ML/'
+data_path='data/' 
+data_path_ML='data/ML/'
 
 #ASWO servers
-data_path='/perm/aswo/data/insitu_python/'
-data_path_ML='/perm/aswo/data/insitu_python/ML_struct_arrays'
+#data_path='/perm/aswo/data/insitu_python/'
+#data_path_ML='/perm/aswo/data/insitu_python/ML_struct_arrays'
 
 
 
