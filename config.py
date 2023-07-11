32880d2106e8ba5eba4b756d378ca0c158ab8fff
cmoestl <chris.moestl@outlook.com>
2023-07-11 10:18:05 +0200

    data update

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ input parameters for heliocats
 
 #local
 data_path='/Users/chris/python/data/insitu_python/' 
-noaa_path='/Users/chris/python/ops/data/noaa'
+noaa_path='/Users/chris/python/ops/data/noaa/'
 data_path_ml='/Users/chris/python/data/insitu_python/ML_struct_arrays/' 
 
 #ASWO servers
 #data_path='/perm/aswo/data/insitu_python/'
-#noaa_path='/perm/aswo/ops/data/noaa'
+#noaa_path='/perm/aswo/ops/data/noaa/'
 #data_path_ml='/perm/aswo/data/insitu_python/ML_struct_arrays/'
 
 
