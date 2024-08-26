26e4258c317f165ae4c591b3b6d6472fca9d777c
cmoestl <chris.moestl@outlook.com>
2024-08-26 17:05:31 +0200

    update science data

--- data_update_web_science.py
+++ data_update_web_science.py
@@ -23,7 +23,7 @@
 # - PSP end date for data downloads needs to be set manually otherwise processing stops on the server - due to a timeout when no output for a while? or entering a loop? PSP data ends 2023 Oct 15, end date set to 2023 Dec 31, need to fix hd.download_pspmag_1min
 # check data availability at: https://spdf.gsfc.nasa.gov/pub/data/psp/fields/l2/mag_rtn_1min
 
-# In[16]:
+# In[19]:
 
 
 # https://github.com/cmoestl/heliocats  data_update_web_science.py
@@ -31,7 +31,7 @@
 # for updating data every day on the servers
 
 #switches
-debug_mode=1
+debug_mode=0
 
 get_omni=1
 get_wind=1 
