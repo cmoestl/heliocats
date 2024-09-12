8ff7ac68765fed0ace5f3d70c5fbf6cca060c653
cmoestl <chris.moestl@outlook.com>
2024-09-12 13:56:31 +0200

    solo data check

--- data_update_web_hf.py
+++ data_update_web_hf.py
@@ -18,7 +18,7 @@
 # 
 # 
 
-# In[2]:
+# In[1]:
 
 
 #switches
@@ -185,7 +185,7 @@ print('Positions and SDO images takes', np.round(t1-t0,2), 'seconds')
 
 # ### NOAA real time solar wind 
 
-# In[5]:
+# In[ ]:
 
 
 if debug_mode > 0: 
@@ -292,7 +292,7 @@ print('NOAA download latest file, save as pickle last 35 files and plotting take
 
 # ### STEREO-A beacon data
 
-# In[6]:
+# In[ ]:
 
 
 if debug_mode > 0: 
@@ -349,7 +349,7 @@ print('STEREO-A beacon downloading last 10 days, save as pickle last 35 days and
 
 # ## Combined plot STEREO-A NOAA RTSW
 
-# In[7]:
+# In[ ]:
 
 
 if debug_mode > 0:     
@@ -362,7 +362,7 @@ hp.plot_insitu_update_stereoa_noaa(noaa, sta_gsm, start, end,'NOAA_RTSW_STEREO-A
     
 
 
-# In[8]:
+# In[ ]:
 
 
 t1all = time.time()
--- data_update_web_science.py
+++ data_update_web_science.py
@@ -26,7 +26,7 @@
 # - PSP end date for data downloads needs to be set manually otherwise processing stops on the server - due to a timeout when no output for a while? or entering a loop? PSP data ends 2023 Oct 15, end date set to 2023 Dec 31, need to fix hd.download_pspmag_1min
 # check data availability at: https://spdf.gsfc.nasa.gov/pub/data/psp/fields/l2/mag_rtn_1min
 
-# In[1]:
+# In[2]:
 
 
 # https://github.com/cmoestl/heliocats  data_update_web_science.py
@@ -36,12 +36,12 @@
 #switches
 debug_mode=0
 
-get_omni=1
-get_wind=1 
-get_psp=1
+get_omni=0
+get_wind=0 
+get_psp=0
 get_solo=1
-get_bepi=1
-get_stereoa=1
+get_bepi=0
+get_stereoa=0
 
 import numpy as np
 import pandas as pd
